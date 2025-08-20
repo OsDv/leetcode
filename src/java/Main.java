8b2ac4dbd0a4bee65ddf8d231a104f774b834a62
Oussama <oussamammm7@gmail.com>
2025-08-20 06:44:04 +0100

    Java: Longest Palindromic Substring
    First problem in the repo

---
+++ src/java/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+class Main 
+{
+	public static void main(String[] args) {
+		String s = "a";
+		LongestPalindromicSubString o = new LongestPalindromicSubString(s);
+		System.out.println(o.result);
+	}
+}
+class LongestPalindromicSubString
+{
+	String s;
+	String result;
+	LongestPalindromicSubString(String s)
+	{
+		this.s = s;
+		result = this.longestPalindrome();
+	}
+
+	private boolean isPalindromic(int start,int legnth)
+	{
+		int i = start;
+		int j = start + legnth -1;
+		while(i<j)
+		{
+			if (s.charAt(i)!=s.charAt(j)) return false;
+			i++;
+			j--;
+		}
+		return true;
+	}
+	private String longestPalindrome()
+	{
+		for (int i=s.length();i>0;i--)
+		{
+			for(int j=0;j<=s.length()-i;j++){
+				System.out.println("i="+i+" ,j="+j);
+				if (this.isPalindromic(j, i)) return s.substring(j, j+i);
+			} 
+		}
+		return null;
+	}
+}
